185f9b89f59c8759420d91fe910e22b5528e091f
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-21 22:35:04 +0100

    [ADD] 3 screens: welcome, homepage and loading page. Fetching OpenWeather data for weather + Button for sharing location + haptic feedback

---
+++ WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,46 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Damien Gautier on 21/03/2023.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the Weather App")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                let impactLight = UIImpactFeedbackGenerator(style: .light)
+                impactLight.impactOccurred()
+                
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
